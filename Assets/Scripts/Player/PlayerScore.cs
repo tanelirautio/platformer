014cdfc5ef997e82d0af0ff23160a02f1513cce7
Taneli Rautio <taneli.rautio@gmail.com>
2021-10-28 00:41:42 +0300

    Update score in UI

--- Assets/Scripts/Player/PlayerScore.cs
+++ Assets/Scripts/Player/PlayerScore.cs
@@ -1,22 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerScore : MonoBehaviour
 {
+    const int MAX_SCORE_CHARACTER_COUNT = 8;
+    const float UI_UPDATE_WAIT_TIME = 0.0001f;
+
     private int currentScore;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public TextMeshProUGUI scoreText;
+    private bool coroutineRunning = false;
 
     public void AddScore(int score)
     {
@@ -24,10 +18,50 @@ public class PlayerScore : MonoBehaviour
 
         //debug
         print("Current score: " + currentScore);
+        UpdateUiScore();
     }
 
     public int GetScore()
     {
         return currentScore;
+    }
+
+    private void UpdateUiScore()
+    {
+        if (!coroutineRunning)
+        {
+            StartCoroutine(UpdateAndWaitUiScore());
+        }
+    }
+
+    private IEnumerator UpdateAndWaitUiScore()
+    {
+        coroutineRunning = true;
+        int currentUiScore = int.Parse(scoreText.text);
+
+        while (currentUiScore != currentScore)
+        {
+            if(currentUiScore < currentScore)
+            {
+                currentUiScore = currentUiScore + 5;
+                if (currentUiScore > currentScore)
+                {
+                    currentUiScore = currentScore;
+                }
+            }
+            else
+            {
+                currentUiScore = currentUiScore - 5;
+                if (currentUiScore < currentScore)
+                {
+                    currentUiScore = currentScore;
+                }
+            }
+            
+            string str = currentUiScore.ToString();
+            scoreText.text = str.PadLeft(MAX_SCORE_CHARACTER_COUNT, '0');
+            yield return null;
+        }
+        coroutineRunning = false;
     }
 }
