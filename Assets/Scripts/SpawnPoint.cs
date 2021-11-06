71af60043a4f0966cdf9f6a33ff5d4bf88266dd2
Taneli Rautio <taneli.rautio@gmail.com>
2021-11-07 00:50:09 +0200

    Animate level start sprite

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
+    private Animator animator;
+    private bool isPlaying = false;
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
@@ -12,5 +15,31 @@ public class SpawnPoint : MonoBehaviour
         //Gizmos.color = Color.yellow;
         //radius = 0.25f;
         //Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public void Reset()
+    {
+        animator.Play("idle");
+        isPlaying = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        print("collision.tag: " + collision.tag);
+
+        if(collision.tag == "Player")
+        {
+            if (!isPlaying)
+            {
+                //TODO: play only once(?)
+                animator.Play("start");
+                isPlaying = true;
+            }
+        }
     }
 }
