aeaca77aa702d89aacc94a2b4e6a663242ad251a
Taneli Rautio <taneli.rautio@gmail.com>
2021-11-19 00:20:02 +0200

    LevelObjectives WIP

--- Assets/Scripts/LevelObjectives.cs
+++ Assets/Scripts/LevelObjectives.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,18 +14,29 @@ public class LevelObjectives : MonoBehaviour
         CollectItem
     }
 
-    private class Objective
-    {
-        private ObjectiveType type;
-        private int amount;
+    private ObjectiveType type;
+    private int amount;
 
-        Objective(ObjectiveType type, int amount)
-        {
-            this.type = type;
-            this.amount = amount;
+    public LevelObjectives(ObjectiveType type, int amount)
+    {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public Dictionary<int, List<LevelObjectives>> objectives = new Dictionary<int, List<LevelObjectives>>()
+    {
+        { 0, new List<LevelObjectives> { new LevelObjectives(ObjectiveType.Points, 300),
+                                         new LevelObjectives(ObjectiveType.Died, 1),
+                                         new LevelObjectives(ObjectiveType.CollectItem, 0) }
+        },
+        { 1, new List<LevelObjectives> { new LevelObjectives(ObjectiveType.Points, 300),
+                                         new LevelObjectives(ObjectiveType.Died, 1),
+                                         new LevelObjectives(ObjectiveType.CollectItem, 0) }
         }
     }
 
+
     // TODO: create 3 objectives for each level
     // At level end, check if objectives met (or if objectives have been met before)
+
 }
