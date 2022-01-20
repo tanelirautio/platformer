62e19949c3ce6e0f46c5ee6da3c79cb46b2046f4
Juha Lehtinen <jtlehtinen@fastmail.com>
2022-01-20 20:06:25 +0200

    remove crap

--- Assets/Scripts/Traps/Trampoline.cs
+++ Assets/Scripts/Traps/Trampoline.cs
@@ -5,12 +5,11 @@ public class Trampoline : MonoBehaviour
     private const string IDLE_ANIMATION_NAME = "trampoline_idle";
     private const string TRIGGER_ANIMATION_NAME = "trampoline_trigger";
 
-    private Animator anim;
-    private float accumulator;
+    private Animator animator;
 
     private void Awake()
     {
-        anim = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     void Start()
@@ -23,9 +22,8 @@ public class Trampoline : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Collided from above
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName(IDLE_ANIMATION_NAME)) {
-            anim.Play(TRIGGER_ANIMATION_NAME);
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName(IDLE_ANIMATION_NAME)) {
+            animator.Play(TRIGGER_ANIMATION_NAME);
         }
     }
 }
