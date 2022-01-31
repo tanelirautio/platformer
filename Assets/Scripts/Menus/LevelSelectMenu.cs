c302bbfb4ec4b42c0335149a1b73873c6d120a2f
Taneli Rautio <taneli.rautio@gmail.com>
2022-01-31 23:53:39 +0200

    Level selection work continues

--- Assets/Scripts/Menus/LevelSelectMenu.cs
+++ Assets/Scripts/Menus/LevelSelectMenu.cs
@@ -13,12 +13,23 @@ namespace pf
         public GameObject container;
         public GameObject scrollArea;
         public GameObject back;
+        public GameObject levelPrefab;
 
         private ScrollRect scrollrect;
         private RectTransform viewport;
 
-        public List<Transform> levelImages = new List<Transform>();
+        public List<Transform> levelIconPositions = new List<Transform>();
+        public List<Sprite> levelImages = new List<Sprite>();
+
+        private int lastCompletedLevel = -1;
+        private List<Transform> levelSelectionObjects = new List<Transform>();
+
+        private PlayerInputActions playerInputActions;
+        private Selection selection = 0;
+        private LevelLoader levelLoader;
+
         private int index = 0;
+        private bool isBottomRow = false; 
 
         public enum Selection
         {
@@ -26,37 +37,36 @@ namespace pf
             Back
         }
 
+        private List<string> levelNames = new List<string>()
+        {
+            "Soul Kitchen",
+            "Ship of Fools",
+            "Blue Sunday"
+        };
+
         private enum ColorInfoKey
         {
-            Achieved,
-            NotAchieved,
-            Selected
+            Open,
+            Closed
         }
 
         private class ColorInfo
         {
-            public ColorInfo(Color bgColor, Color imgColor, Color textColor)
+            public ColorInfo(Color bgColor, Color trophyColor, Color textColor)
             {
                 BgColor = bgColor;
-                ImgColor = imgColor;
+                TrophyColor = trophyColor;
                 TextColor = textColor;
             }
-
             public Color BgColor { get; private set; }
-            public Color ImgColor { get; private set; }
+            public Color TrophyColor { get; private set; }
             public Color TextColor { get; private set; }
         }
 
         private Dictionary<ColorInfoKey, ColorInfo> colors = new Dictionary<ColorInfoKey, ColorInfo>() {
-            { ColorInfoKey.Achieved, new ColorInfo(new Color (192f / 255f, 50f / 255f, 127f / 255f), new Color(1,1,1), new Color(1,1,1)) },
-            { ColorInfoKey.NotAchieved, new ColorInfo(Color.gray, Color.gray, Color.white) },
-            { ColorInfoKey.Selected, new ColorInfo(new Color(1,1,1), new Color(100f / 255f, 100f / 255f, 100f / 255f), new Color(0,0,0)) }
+            { ColorInfoKey.Open, new ColorInfo(Color.white, new Color(1, 234f / 255f, 0), Color.white) },
+            { ColorInfoKey.Closed, new ColorInfo(new Color(105f / 255f, 105f / 255f, 105f / 255f), new Color(48f / 255f, 48f / 255f, 48f / 255f), new Color(48f / 255f, 48f / 255f, 48f / 255f)) }
         };
-
-
-        private PlayerInputActions playerInputActions;
-        private Selection selection = 0;
-        private LevelLoader levelLoader;
 
         private void Awake()
         {
@@ -71,115 +81,123 @@ namespace pf
         {
             DataLoader.ParseData();
 
-            //float offset = 8f;
-
-            for(int i = 0; i < levelImages.Count; i++)
+            for (int i = 0; i < levelIconPositions.Count; i++)
             {
-                if(i < Defs.LEVEL_AMOUNT)
-                {
-                    Image trophy0 = levelImages[i].Find("TrophyBg/Trophy0").GetComponent<Image>();
-                    Image trophy1 = levelImages[i].Find("TrophyBg/Trophy1").GetComponent<Image>();
-                    Image trophy2 = levelImages[i].Find("TrophyBg/Trophy2").GetComponent<Image>();
+                GameObject go = Instantiate(levelPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                go.transform.SetParent(container.transform, false);
+                go.name = levelPrefab.name + "_" + i;
+                go.transform.position = levelIconPositions[i].position;
 
+
+                if(i < levelImages.Count )
+                {
+                    go.GetComponent<Image>().sprite = levelImages[i];
+                }
+
+                TextMeshProUGUI name = go.transform.Find("Name").GetComponent<TextMeshProUGUI>();
+                if (i < levelNames.Count)
+                {
+                    name.text = levelNames[i];
+                }
+
+                Image trophy0 = go.transform.Find("TrophyBg/Trophy0").GetComponent<Image>();
+                Image trophy1 = go.transform.Find("TrophyBg/Trophy1").GetComponent<Image>();
+                Image trophy2 = go.transform.Find("TrophyBg/Trophy2").GetComponent<Image>();
+                if (i < Defs.LEVEL_AMOUNT)
+                {
                     if (PlayerStats.LevelsCompleted[i])
                     {
-                        if(PlayerStats.CompletedObjectives[i].CompletedNoHits)
-                        {
-                            trophy0.color = new Color(1, 234f / 255f, 0);
-                        }
-                        else
-                        {
-                            trophy0.color = new Color(48f/255f, 48f / 255f, 48f/255f);
-                        }
-
-                        if(PlayerStats.CompletedObjectives[i].CompletedPoints)
-                        {
-                            trophy1.color = new Color(1, 234f / 255f, 0);
-                        }
-                        else
-                        {
-                            trophy1.color = new Color(48f / 255f, 48f / 255f, 48f / 255f);
-                        }
-
-                        if (PlayerStats.CompletedObjectives[i].CompletedTime)
-                        {
-                            trophy2.color = new Color(1, 234f / 255f, 0);
-                        }
-                        else
-                        {
-                            trophy2.color = new Color(48f / 255f, 48f / 255f, 48f / 255f);
-                        }
+                        SetLevelOpen(go, i, trophy0, trophy1, trophy2, name);
+                        lastCompletedLevel = i;
                     }
                     else
                     {
-                        if (i != 0) // first level is always shown as open
-                        {
-                            levelImages[i].GetComponent<Image>().color = new Color(105f / 255f, 105f / 255f, 105f / 255f);
-                        }
-                        trophy0.color = new Color(48f / 255f, 48f / 255f, 48f / 255f);
-                        trophy1.color = new Color(48f / 255f, 48f / 255f, 48f / 255f);
-                        trophy2.color = new Color(48f / 255f, 48f / 255f, 48f / 255f);
+                        SetLevelClosed(go, trophy0, trophy1, trophy2, name);
                     }
                 }
                 else
                 {
-                    levelImages[i].GetComponent<Image>().color = new Color(105f/255f, 105f/255f, 105f/255f);
+                    //Note: Code shouldn't go here ever after all the levels are finalized
+                    SetLevelClosed(go, trophy0, trophy1, trophy2, name);
                 }
+
+                levelSelectionObjects.Add(go.transform);
             }
 
-            /*
-            for (int i = 0; i < PlayerStats.Achievements.Count; i++)
+            print("LevelSelectionObjects.Count: " + levelSelectionObjects.Count);
+
+            // Open the first not completed level to be played
+            if (lastCompletedLevel < Defs.LEVEL_AMOUNT)
             {
+                int i = lastCompletedLevel + 1;
+                index = i;
+                print("Opening index: " + i);
 
-                GameObject go = Instantiate(achievement, new Vector3(0, 0, 0), Quaternion.identity);
-                go.transform.SetParent(container.transform, false);
-                go.name = achievement.name + "_" + i;
-                Vector3 pos = go.transform.position;
-                pos.y = offset - i * 3f;
-                go.transform.position = pos;
-
-                TextMeshProUGUI title = go.transform.Find("AchieveTitle").GetComponent<TextMeshProUGUI>();
-                title.text = PlayerStats.Achievements[i].title;
-
-                TextMeshProUGUI desc = go.transform.Find("AchieveDesc").GetComponent<TextMeshProUGUI>();
-                desc.text = PlayerStats.Achievements[i].desc;
-
-
-                //print("Setting position " + i + ": " + go.transform.position);
-                if (!PlayerStats.CompletedAchievements[i])
+                if (i < levelSelectionObjects.Count)
                 {
-                    ColorizePanel(go.transform, ColorInfoKey.NotAchieved);
+                    GameObject go = levelSelectionObjects[i].gameObject;
+                    Image trophy0 = go.transform.Find("TrophyBg/Trophy0").GetComponent<Image>();
+                    Image trophy1 = go.transform.Find("TrophyBg/Trophy1").GetComponent<Image>();
+                    Image trophy2 = go.transform.Find("TrophyBg/Trophy2").GetComponent<Image>();
+                    TextMeshProUGUI name = go.transform.Find("Name").GetComponent<TextMeshProUGUI>();
+                    SetLevelOpen(go, i, trophy0, trophy1, trophy2, name);
+
+                    levelSelectionObjects[i].DOScale(Defs.MENU_SELECTED_SCALE, 0f);
+                    levelSelectionObjects[i].Find("Selection").gameObject.SetActive(true);
                 }
-                
-                achievements.Add(go.transform);
             }
-            */
-
-            //achievements[0].localScale = new Vector3(1.1f, 1.1f, 1.1f);
-
-            //achievements[0].DOScale(Defs.MENU_SELECTED_SCALE, 0f);
-            /*
-            if (!PlayerStats.CompletedAchievements[index])
+            else
             {
-                ColorizePanel(achievements[index], ColorInfoKey.Selected);
+                print("ALL LEVELS OPEN - just select the last one");
+                levelSelectionObjects[Defs.LEVEL_AMOUNT - 1].DOScale(Defs.MENU_SELECTED_SCALE, 0f);
+                levelSelectionObjects[Defs.LEVEL_AMOUNT - 1].Find("Selection").gameObject.SetActive(true);
+                index = Defs.LEVEL_AMOUNT - 1;
             }
-            */
 
             back.GetComponent<SpriteRenderer>().color = Color.gray;
-
-
         }
 
-        private void ColorizePanel(Transform t, ColorInfoKey key)
+        private void SetLevelOpen(GameObject go, int i, Image trophy0, Image trophy1, Image trophy2, TextMeshProUGUI name)
         {
-            /*
-            t.gameObject.GetComponent<Image>().color = colors[key].BgColor;
-            t.Find("AchieveImgBg/AchieveImg").GetComponent<Image>().color = colors[key].ImgColor;
-            var titleText = t.Find("AchieveTitle").GetComponent<TextMeshProUGUI>();
-            titleText.DOColor(colors[key].TextColor, 0.01f);
-            var descText = t.Find("AchieveDesc").GetComponent<TextMeshProUGUI>();
-            descText.DOColor(colors[key].TextColor, 0.01f);
-            */
+            go.transform.GetComponent<Image>().color = colors[ColorInfoKey.Open].BgColor;
+            name.color = colors[ColorInfoKey.Open].TextColor;
+
+
+            if (PlayerStats.CompletedObjectives[i].CompletedNoHits)
+            {
+                trophy0.color = colors[ColorInfoKey.Open].TrophyColor;
+            }
+            else
+            {
+                trophy0.color = colors[ColorInfoKey.Closed].TrophyColor;
+            }
+
+            if (PlayerStats.CompletedObjectives[i].CompletedPoints)
+            {
+                trophy1.color = colors[ColorInfoKey.Open].TrophyColor;
+            }
+            else
+            {
+                trophy1.color = colors[ColorInfoKey.Closed].TrophyColor;
+            }
+
+            if (PlayerStats.CompletedObjectives[i].CompletedTime)
+            {
+                trophy2.color = colors[ColorInfoKey.Open].TrophyColor;
+            }
+            else
+            {
+                trophy2.color = colors[ColorInfoKey.Closed].TrophyColor;
+            }
+        }
+
+        private void SetLevelClosed(GameObject go, Image trophy0, Image trophy1, Image trophy2, TextMeshProUGUI name)
+        {
+            go.transform.GetComponent<Image>().color = colors[ColorInfoKey.Closed].BgColor;
+            trophy0.color = colors[ColorInfoKey.Closed].TrophyColor;
+            trophy1.color = colors[ColorInfoKey.Closed].TrophyColor;
+            trophy2.color = colors[ColorInfoKey.Closed].TrophyColor;
+            name.color = colors[ColorInfoKey.Closed].TextColor;
         }
 
         private void Navigate(RectTransform item)
@@ -242,70 +260,101 @@ namespace pf
 
             if (selection == Selection.Scroll)
             {
-                if (deltaX == 1)
-                {
-                    selection = Selection.Back;
-                    back.GetComponent<SpriteRenderer>().color = Color.white;
-                    /*achievements[index].DOScale(Defs.MENU_NORMAL_SCALE, 1f);
-                    if (!PlayerStats.CompletedAchievements[index])
-                    {
-                        ColorizePanel(achievements[index], ColorInfoKey.NotAchieved);
-                    }*/
-                }
-                else if (deltaY != 0)
-                {
+                int prevIndex = index;
 
-                    /*
-                    int prevIndex = index;
-
-                    if (deltaY == 1)
+                if (deltaX != 0)
+                {
+                    if (deltaX == 1)
                     {
-                        if (index < achievements.Count - 1)
+                        if (index == 0)
                         {
-                            index++;
+                            selection = Selection.Back;
+                            back.GetComponent<SpriteRenderer>().color = Color.white;
+                            levelSelectionObjects[index].DOScale(Defs.MENU_NORMAL_SCALE, 1f);
+                            levelSelectionObjects[index].Find("Selection").gameObject.SetActive(false);
+                        }
+                        else
+                        {
+                            index = index - 1;
+                            if (index == 4 || index == 14)
+                            {
+                                isBottomRow = false;
+                            }
+                            if(index == 9)
+                            {
+                                isBottomRow = true;
+                            }
                         }
                     }
                     else
                     {
-                        if (index > 0)
+                        index = index + 1;
+                        if (index == 5 || index == 15)
                         {
-                            index--;
+                            isBottomRow = true;
+                        }
+                        if(index == 10)
+                        {
+                            isBottomRow = false;
                         }
                     }
-
-                    if (prevIndex != index)
-                    {
-                        achievements[prevIndex].DOScale(Defs.MENU_NORMAL_SCALE, 1f);
-                        if (!PlayerStats.CompletedAchievements[prevIndex])
-                        {
-                            ColorizePanel(achievements[prevIndex], ColorInfoKey.NotAchieved);
-                        }
-                        achievements[index].DOScale(Defs.MENU_SELECTED_SCALE, 1f);
-                        if (!PlayerStats.CompletedAchievements[index])
-                        {
-                            ColorizePanel(achievements[index], ColorInfoKey.Selected);
-                        }
-                    }
-
-                    if (!RendererExtensions.IsFullyVisibleFrom(achievements[index].GetComponent<RectTransform>(), Camera.main))
-                    {
-                        Navigate(achievements[index].GetComponent<RectTransform>());
-                    }
-                    */
                 }
+                else if (deltaY != 0)
+                {
+                    if (deltaY == 1)
+                    {
+                        print("press down");
+                        if (!isBottomRow)
+                        {
+                            index = index + 5;
+                            isBottomRow = true;
+                        }
+                    }
+                    else
+                    {
+                        print("press up");
+                        if (isBottomRow)
+                        {
+                            index = index - 5;
+                            isBottomRow = false;
+                        }
+                        else
+                        {
+                            selection = Selection.Back;
+                            back.GetComponent<SpriteRenderer>().color = Color.white;
+                            levelSelectionObjects[index].DOScale(Defs.MENU_NORMAL_SCALE, 1f);
+                            levelSelectionObjects[index].Find("Selection").gameObject.SetActive(false);
+                        }
+                    }
+                }
+
+                if (selection == Selection.Scroll)
+                {
+                    index = Mathf.Clamp(index, 0, levelSelectionObjects.Count - 1);
+                    print("ALRIGHTY THEN: index is: " + index);
+
+                    levelSelectionObjects[prevIndex].DOScale(Defs.MENU_NORMAL_SCALE, 1f);
+                    levelSelectionObjects[prevIndex].Find("Selection").gameObject.SetActive(false);
+
+                    levelSelectionObjects[index].DOScale(Defs.MENU_SELECTED_SCALE, 1f);
+                    levelSelectionObjects[index].Find("Selection").gameObject.SetActive(true);
+                }
+
+                // TODO: Change this to scroll to show all the level pack levels
+                if (!RendererExtensions.IsFullyVisibleFrom(levelSelectionObjects[index].GetComponent<RectTransform>(), Camera.main))
+                {
+                    Navigate(levelSelectionObjects[index].GetComponent<RectTransform>());
+                }
+                
             }
             else if (selection == Selection.Back)
             {
-                if (deltaX == -1)
+                if (deltaX == -1 || deltaY == 1)
                 {
                     selection = Selection.Scroll;
                     back.GetComponent<SpriteRenderer>().color = Color.gray;
-                    /*achievements[index].DOScale(Defs.MENU_SELECTED_SCALE, 1f);
-                    if (!PlayerStats.CompletedAchievements[index])
-                    {
-                        ColorizePanel(achievements[index], ColorInfoKey.Selected);
-                    }
-                    */
+                    levelSelectionObjects[index].DOScale(Defs.MENU_SELECTED_SCALE, 1f);
+                    levelSelectionObjects[index].Find("Selection").gameObject.SetActive(true);
                 }
             }
         }
