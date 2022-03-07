6d855dc271a7dadfd1f2ab8b78cb2ff46ca1f5be
Taneli Rautio <taneli.rautio@gmail.com>
2022-03-08 00:14:03 +0200

    Falling platform idle movement & falling when player detected working (still missing: going back up when player leaves)

--- Assets/Scripts/Traps/FallingPlatform.cs
+++ Assets/Scripts/Traps/FallingPlatform.cs
@@ -5,90 +5,323 @@ using DG.Tweening;
 
 namespace pf
 {
+    public class FallingPlatform : RaycastController
+    { 
+        public LayerMask passengerMask;
 
-    //TODO: Make this more platform controller like, check with raycasts if player is on top of the platform and run the logic based on that information. 
-    //Triggering platform via OnTriggerEnter2D and OnTriggerExit2D will lead to bugs (collision from under the platform, player jumping off platform before it falls etc.)
 
-    public class FallingPlatform : MonoBehaviour
-    {
-        private Animator anim;
+        public Vector3[] localIdleWaypoints = new Vector3[2];
+        private Vector3[] globalIdleWaypoints;
 
-        public float fallingHeight = 4.0f;
-        public float fallingTime = 0.5f;
+        public Vector3 localFallingWaypoint;
+        private Vector3[] globalFallingWaypoints;
 
-        private float startY;
+        Vector3 fallStartPosition;
 
-        private enum State
+        public float speed;
+        public float fallingSpeed;
+
+        public float waitTime;
+        [Range(0, 2)]
+        public float easeAmount;
+
+        int fromWaypointIndex;
+        float percentBetweenWaypoints;
+        float nextMoveTime;
+
+
+
+        List<PassengerMovement> passengerMovement;
+        Dictionary<Transform, Controller2D> passengerDictionary = new Dictionary<Transform, Controller2D>();
+
+        enum State
         {
-            Up,
-            GoingDown,
-            Down,
-            GoingUp
+            Idle,
+            Falling,
+            Bottom,
+            GoingUp,
         }
-        private State currentState = State.Up;
+        State state = State.Idle;
 
-        //animation states
-        const string FALLING_PLATFORM_ON = "falling_platform_on";
-        const string FALLING_PLATFORM_OFF = "falling_platform_off";
-
-        private void Awake()
+        public override void Start()
         {
-            anim = GetComponent<Animator>();
-        }
+            base.Start();
 
-        void Start()
-        {
-            PlayAnimation(FALLING_PLATFORM_ON);
-            startY = transform.position.y;
-        }
+            // Waypoints that the platform goes when player not on top
+            globalIdleWaypoints = new Vector3[2];
+            globalIdleWaypoints[0] = localIdleWaypoints[0] + transform.position;
+            globalIdleWaypoints[1] = localIdleWaypoints[1] + transform.position;
 
+            // Waypoints that the platform goes when player on top (will continue stay in localFallingWaypoint)
+            globalFallingWaypoints = new Vector3[2];
+            globalFallingWaypoints[0] = localIdleWaypoints[0] + transform.position;
+            globalFallingWaypoints[1] = localFallingWaypoint + transform.position;
+        }
 
         void Update()
         {
+            UpdateRaycastOrigins();
+            Vector3 velocity = Vector3.zero;
 
-        }
-
-        public void Trigger(bool fall)
-        {
-            if(fall && currentState == State.Up)
+            if (state != State.Falling && PlayerDetectedOnPlatform())
             {
-                Invoke("Fall", Defs.TRAP_FALLING_PLATFORM_WAIT_TIME);
+                //TODO: start timer, fall the platform (change state) after 0.5 seconds or smth
+                state = State.Falling;
+            }     
+            
+            // Always fall to bottom when State.Falling is set
+            if(state == State.Falling) {
+                velocity = CalculatePlatformFallMovement(true);
             }
-            else if(!fall && currentState == State.Down)
+            else if(state == State.Idle)
             {
-                GoUp();
+                velocity = CalculatePlatformIdleMovement();                 
+            }
+
+            //TODO: detect player leaving the platform and move up
+            //only after moving back to globalIdleWaypoint[0] change state back to State.Idle
+            
+            CalculatePassengerMovement(velocity);
+            MovePassengers(true);
+            transform.Translate(velocity);
+            MovePassengers(false);
+        }
+
+        private bool PlayerDetectedOnPlatform()
+        {
+            float rayLength = SKINWIDTH * 2;
+
+            for (int i = 0; i < verticalRayCount; i++)
+            {
+                Vector2 rayOrigin = raycastOrigins.topLeft;
+                rayOrigin += Vector2.right * (verticalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, passengerMask);
+
+                Debug.DrawLine(rayOrigin, rayOrigin + Vector2.up * rayLength, Color.blue);
+
+                if (hit && hit.distance != 0)
+                {         
+                    if(fallStartPosition == Vector3.zero)
+                    {
+                        fallStartPosition = transform.position;
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        float Ease(float x)
+        {
+            float a = easeAmount + 1;
+            return Mathf.Pow(x, a) / (Mathf.Pow(x, a) + Mathf.Pow(1 - x, a));
+        }
+
+        Vector3 CalculatePlatformIdleMovement()
+        {
+            if (Time.time < nextMoveTime)
+            {
+                return Vector3.zero;
+            }
+
+            fromWaypointIndex %= globalIdleWaypoints.Length;
+            int toWaypointIndex = (fromWaypointIndex + 1) % globalIdleWaypoints.Length;
+            float distanceBetweenWaypoints = Vector3.Distance(globalIdleWaypoints[fromWaypointIndex], globalIdleWaypoints[toWaypointIndex]);
+            percentBetweenWaypoints += Time.deltaTime * speed / distanceBetweenWaypoints;
+            percentBetweenWaypoints = Mathf.Clamp01(percentBetweenWaypoints);
+            float easedPercentBetweenWaypoints = Ease(percentBetweenWaypoints);
+
+            Vector3 newPos = Vector3.Lerp(globalIdleWaypoints[fromWaypointIndex], globalIdleWaypoints[toWaypointIndex], easedPercentBetweenWaypoints);
+
+            if (percentBetweenWaypoints >= 1)
+            {
+                percentBetweenWaypoints = 0;
+                fromWaypointIndex++;
+                nextMoveTime = Time.time + waitTime;
+            }
+
+            return newPos - transform.position;
+        }
+
+
+
+        Vector3 CalculatePlatformFallMovement(bool falling)
+        {
+            if (falling)
+            {
+                float distanceBetweenWaypoints = Vector3.Distance(fallStartPosition, globalFallingWaypoints[1]);
+                percentBetweenWaypoints += Time.deltaTime * fallingSpeed / distanceBetweenWaypoints;
+                percentBetweenWaypoints = Mathf.Clamp01(percentBetweenWaypoints);
+                float easedPercentBetweenWaypoints = Ease(percentBetweenWaypoints);
+
+                Vector3 newPos = Vector3.Lerp(fallStartPosition, globalFallingWaypoints[1], easedPercentBetweenWaypoints);
+                return newPos - transform.position;
+            }
+
+            /*
+            else
+            {
+                fromWaypointIndex %= globalWaypoints.Length;
+                float distanceBetweenWaypoints = Vector3.Distance(globalFallingWaypoint, globalWaypoints[fromWaypointIndex]);
+                percentBetweenWaypoints += Time.deltaTime * speed / distanceBetweenWaypoints;
+                percentBetweenWaypoints = Mathf.Clamp01(percentBetweenWaypoints);
+                float easedPercentBetweenWaypoints = Ease(percentBetweenWaypoints);
+
+                Vector3 newPos = Vector3.Lerp(globalFallingWaypoint, globalWaypoints[fromWaypointIndex], easedPercentBetweenWaypoints);
+
+                if (percentBetweenWaypoints >= 1) 
+                {
+                    state = State.Idle;
+                }
+                return newPos - transform.position;
+            }   
+            */
+            return Vector3.zero;
+        }
+
+        void MovePassengers(bool beforeMovePlatform)
+        {
+            foreach (PassengerMovement passenger in passengerMovement)
+            {
+                if (!passengerDictionary.ContainsKey(passenger.transform))
+                {
+                    passengerDictionary.Add(passenger.transform, passenger.transform.GetComponent<Controller2D>());
+                }
+                if (passenger.moveBeforePlatform == beforeMovePlatform)
+                {
+                    passengerDictionary[passenger.transform].Move(passenger.velocity, passenger.standingOnPlatform);
+                }
             }
         }
 
-        private void Fall()
+        void CalculatePassengerMovement(Vector3 velocity)
         {
-            PlayAnimation(FALLING_PLATFORM_OFF);
-            currentState = State.GoingDown;
-            transform.DOMoveY(startY - fallingHeight, fallingTime).OnComplete(FallComplete);
+            HashSet<Transform> movedPassengers = new HashSet<Transform>();
+            passengerMovement = new List<PassengerMovement>();
+
+            float directionX = Mathf.Sign(velocity.x);
+            float directionY = Mathf.Sign(velocity.y);
+
+            // Vertically moving platform
+            /*
+            if (velocity.y != 0)
+            {
+                float rayLength = Mathf.Abs(velocity.y) + SKINWIDTH;
+
+                for (int i = 0; i < verticalRayCount; i++)
+                {
+                    Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+                    rayOrigin += Vector2.right * (verticalRaySpacing * i);
+                    RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, passengerMask);
+
+                    //Debug.DrawRay(rayOrigin, Vector2.up * directionY, Color.blue);
+
+                    if (hit && hit.distance != 0)
+                    {
+                        if (!movedPassengers.Contains(hit.transform))
+                        {
+                            movedPassengers.Add(hit.transform);
+                            float pushX = (directionY == 1) ? velocity.x : 0;
+                            float pushY = velocity.y - (hit.distance - SKINWIDTH) * directionY;
+
+                            passengerMovement.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), directionY == 1, true));
+                        }
+                    }
+                }
+            }
+            */
+
+            /*
+
+            // Horizontally moving platform
+            if (velocity.x != 0)
+            {
+                float rayLength = Mathf.Abs(velocity.x) + SKINWIDTH;
+
+                for (int i = 0; i < horizontalRayCount; i++)
+                {
+                    Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+                    rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+                    RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, passengerMask);
+
+                    if (hit && hit.distance != 0)
+                    {
+                        if (!movedPassengers.Contains(hit.transform))
+                        {
+                            movedPassengers.Add(hit.transform);
+                            float pushX = velocity.x - (hit.distance - SKINWIDTH) * directionX;
+                            float pushY = -SKINWIDTH;
+
+                            passengerMovement.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), false, true));
+                        }
+                    }
+                }
+
+            }
+            */
+
+            // Passenger on top of a horizontally or downward moving platform
+            //if (directionY == -1 || velocity.y == 0 && velocity.x != 0)
+            {
+                float rayLength = SKINWIDTH * 2;
+
+                for (int i = 0; i < verticalRayCount; i++)
+                {
+                    Vector2 rayOrigin = raycastOrigins.topLeft += Vector2.right * (verticalRaySpacing * i);
+                    RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, passengerMask);
+
+                    if (hit && hit.distance != 0)
+                    {
+                        if (!movedPassengers.Contains(hit.transform))
+                        {
+                            movedPassengers.Add(hit.transform);
+                            float pushX = velocity.x;
+                            float pushY = velocity.y;
+
+
+                            passengerMovement.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), true, false));
+                        }
+                    }
+                }
+            }
+
+            
         }
 
-        private void FallComplete()
+        struct PassengerMovement
         {
-            PlayAnimation(FALLING_PLATFORM_ON);
-            currentState = State.Down;
+            public Transform transform;
+            public Vector3 velocity;
+            public bool standingOnPlatform;
+            public bool moveBeforePlatform;
+
+            public PassengerMovement(Transform _transform, Vector3 _velocity, bool _standingOnPlatform, bool _moveBeforePlatform)
+            {
+                transform = _transform;
+                velocity = _velocity;
+                standingOnPlatform = _standingOnPlatform;
+                moveBeforePlatform = _moveBeforePlatform;
+            }
         }
 
-        private void PlayAnimation(string animation)
+        private void OnDrawGizmos()
         {
-            anim.Play(animation);
-        }
+            if (localIdleWaypoints != null)
+            {
+                Gizmos.color = Color.red;
+                float size = 0.3f;
+                for (int i = 0; i < localIdleWaypoints.Length; i++)
+                {
+                    Vector3 globalWaypointPos = (Application.isPlaying) ? globalIdleWaypoints[i] : localIdleWaypoints[i] + transform.position;
+                    Gizmos.DrawLine(globalWaypointPos - Vector3.up * size, globalWaypointPos + Vector3.up * size);
+                    Gizmos.DrawLine(globalWaypointPos - Vector3.left * size, globalWaypointPos + Vector3.left * size);
+                }
 
-        private void GoUp()
-        {
-            PlayAnimation(FALLING_PLATFORM_ON);
-            transform.DOMoveY(startY, fallingTime).OnComplete(MoveUpComplete);
-            currentState = State.GoingUp;
-        }
-
-        private void MoveUpComplete()
-        {
-            currentState = State.Up;
+                Gizmos.color = Color.green;
+                Vector3 globalFallingWaypointPos = (Application.isPlaying) ? globalFallingWaypoints[1] : localFallingWaypoint + transform.position;
+                Gizmos.DrawLine(globalFallingWaypointPos - Vector3.up * size, globalFallingWaypointPos + Vector3.up * size);
+                Gizmos.DrawLine(globalFallingWaypointPos - Vector3.left * size, globalFallingWaypointPos + Vector3.left * size);
+            }
         }
     }
-
 }
