87359f1b949248cfbb4d8a6d5502897fbf428066
Taneli Rautio <taneli.rautio@gmail.com>
2022-06-09 00:37:25 +0300

    More spiked ball

--- Assets/Scripts/Traps/SpikedBall.cs
+++ Assets/Scripts/Traps/SpikedBall.cs
@@ -1,21 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 namespace pf
 {
     public class SpikedBall : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        public enum Direction
         {
-        
+            Right,
+            Left,
         }
 
-        // Update is called once per frame
-        void Update()
+        private Direction dir = Direction.Right;
+
+        public float speed = 100f;
+        public int angleLimit = 0;
+     
+        void Start()
         {
-            transform.Rotate(new Vector3(0, 0, Time.deltaTime * 100));
+            if (angleLimit != 0)
+            {
+                RotateRight();
+            }
+        }
+
+
+        void RotateRight()
+        {
+            dir = Direction.Right;
+            transform.DORotate(new Vector3(0, 0, angleLimit), speed / 60f).OnComplete(RotateStart).SetEase(Ease.Linear);
+        }
+
+        void RotateStart()
+        {
+            if (dir == Direction.Right)
+            {
+                transform.DORotate(new Vector3(0, 0, 0), speed / 60f).OnComplete(RotateLeft).SetEase(Ease.Linear);
+            }
+            else
+            {
+                transform.DORotate(new Vector3(0, 0, 0), speed / 60f).OnComplete(RotateRight).SetEase(Ease.Linear);
+            }
+
+        }
+
+        void RotateLeft()
+        {
+            dir = Direction.Left;
+            transform.DORotate(new Vector3(0,0,-angleLimit), speed/60f).OnComplete(RotateStart).SetEase(Ease.Linear);
+        }
+
+        public void Update()
+        {
+            if(angleLimit == 0)
+            {
+                transform.Rotate(new Vector3(0, 0, Time.deltaTime * speed));
+            }
+        }
+        private void OnDestroy()
+        {
+            DOTween.KillAll();
         }
     }
 }
